e71a84f7865e65046c0ac40e6fc19b508f6b2aaa
Hocceruser <Hocceruser@users.noreply.github.com>
2017-05-09 18:49:01 +0200

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,4 @@
+# Ihr müsst ein neues Projekt in Eclipse anlegen. Dann wechselt ihr mit 
+cd workspace/EUERPROJEKTNAME/src
+# in den src-Ordner und führt folgenden Befehl aus:
+git clone https://github.com/au1905/jumpnrungame.git && cp jumpnrungame/* . && rm -rf jumpnrungame
