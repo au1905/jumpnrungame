acf2ce56334fe55f89299a6b11885efb4b9064f1
Hocceruser <hocceruser-github@gmx.de>
2017-05-16 19:29:01 +0200

    Skalierung von HindernisTreppe verändert

--- HindernisTreppe.cpp
+++ HindernisTreppe.cpp
@@ -8,7 +8,7 @@
 #include "HindernisTreppe.h"
 
 HindernisTreppe::HindernisTreppe(double x, double y, double size, GLuint texturNr) :
-		Hindernisse(x, y, size*2, texturNr) {
+		Hindernisse(x, y, size*2.0, size*1.0, texturNr) {
 	// TODO Auto-generated constructor stub
 
 }
